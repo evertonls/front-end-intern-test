f0434ddaf5cc81f89013abe5bc06677c4f1c2464
Everton L <tinholeandro94@gmail.com>
2019-08-22 17:31:52 -0300

    Teste Advance

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
-// Adicionamos nosso código dentro de uma função anônima para evitar
-// variáveis globais.
-(function() {
-  // Elemento principal do formulário
+(function () {
   const contactForm = document.getElementById('contact');
-
-  // Campos do formulário
   const nameInput = document.getElementById('name');
   const emailInput = document.getElementById('email');
   const messageInput = document.getElementById('message');
+  var erro = false;
 
-  // "addEventListener" instrui o navegador a executar uma função sempre
-  // que um evento específico acontece. Nesse caso, a função abaixo será
-  // executada toda vez que o botão de envio for pressionado.
-  contactForm.addEventListener('submit', function() {
-    // A propriedade "value" contém o valor atual de um input ou textarea
-    const nameValue = nameInput.value;
+    contactForm.addEventListener('submit', function (evt) {
 
-    // A propriedade "length" contém o tamanho (em caracteres) de uma
-    // uma string. Toda string contém essa propriedade.
-    const nameLength = nameValue.length;
+      if(nameInput.value == ""){
 
-    // "window" é um objeto global que representa a janela (ou aba) do
-    // navegador que está executando o código do seu site. O método
-    // "alert" simplesmente mostra um aviso para o usuário contendo a
-    // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
+        window.alert('Por favor preencha o campo Nome.');
+        document.getElementById('name').focus();
+        erro = false;
 
-    // Altere e complete essa função para validar os campos do formulário
-    // de acordo com as especificações do teste. Boa sorte!
-  });
+      }
+      else if(emailInput.value == ""){
+
+        window.alert('Por favor preencha o campo Email. ');
+        document.getElementById('email').focus();
+        erro = false;
+
+      }
+      else if(emailInput.value.indexOf('@') == -1){
+
+        window.alert('Email invalido.');
+        document.getElementById('email').focus();
+        erro = false;
+
+      }
+      else if(messageInput.value == ""){
+
+        window.alert('Por favor preencha o campo Mensagem.');
+        document.getElementById('email').focus();
+        erro = false;
+
+      }
+      else {
+
+        window.alert('Parabéns sua mensagem foi enviada com sucesso!');
+        erro = true;
+
+      }
+      if(!erro){
+
+        evt.preventDefault();
+        
+      }
+    });
 })();
